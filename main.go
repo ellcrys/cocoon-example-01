833edb9a0c93b82acbb0a38745494c49a8c7a5c3
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-16 15:10:01 +0100

    proper format

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Hello Friend")
-	output, err := exec.Command("/bin/sh", "-c", "curl localhost:8500/v1/catalog/services").Output()
+	output, err := exec.Command("/bin/sh", "-c", "'curl localhost:8500/v1/catalog/services'").Output()
 	if err != nil {
 		fmt.Println("Err:", err, string(output))
 		return
