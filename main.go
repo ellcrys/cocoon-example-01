4ca8588b695ee5ef2738915c7014d74bef8c6a10
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-16 15:03:21 +0100

    access consul

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 func main() {
 	fmt.Println("Hello Friend")
+	output, err := exec.Command("/bin/sh", "-c", "curl localhost:8500/v1/catalog/services").Output()
+	if err != nil {
+		fmt.Println("Err:", err, string(output))
+		return
+	}
+
+	fmt.Println(string(output))
+
 }
