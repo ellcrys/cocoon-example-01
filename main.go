f1e5b5c80e4f3d0e1373152d4012fc88cbfb6004
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-16 16:19:18 +0100

    fix commad

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Hello Friend")
-	output, err := exec.Command("/bin/sh", "-c", "'curl http://127.0.0.1'").Output()
+	output, err := exec.Command("curl http://127.0.0.1:3000").Output()
 	if err != nil {
 		fmt.Println("Err:", err, string(output))
 		return
