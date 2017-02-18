dd704d2cb4b5ea4d062602e40d9e7e31f4ba7a9d
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-18 11:06:15 +0100

    try to pull page

--- main.go
+++ main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	output, err := exec.Command("bash", "-c", "iptables -S").Output()
-	if err != nil {
-		fmt.Println("Err:", err, string(output))
-		return
-	}
-
-	fmt.Println(len(output), string(output))
-
-	// res, err := http.Get("http://localhost:8500/v1/catalog/services")
+	// output, err := exec.Command("bash", "-c", "iptables -S").Output()
 	// if err != nil {
-	// 	fmt.Println("Err: ", err)
+	// 	fmt.Println("Err:", err, string(output))
 	// 	return
 	// }
 
-	// defer res.Body.Close()
-	// fmt.Println(res.StatusCode)
-	// _, err = io.Copy(os.Stdout, res.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// fmt.Println(len(output), string(output))
+
+	res, err := http.Get("https://google.com.ng")
+	if err != nil {
+		fmt.Println("Err: ", err)
+		return
+	}
+
+	defer res.Body.Close()
+	fmt.Println(res.StatusCode)
+	_, err = io.Copy(os.Stdout, res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
