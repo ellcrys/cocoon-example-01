885b9c2d6a6118995a1bb0711ab2c03e026bcab2
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-18 09:44:56 +0100

    run iptables command

--- main.go
+++ main.go
@@ -2,33 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"os"
-
-	"net/http"
+	"os/exec"
 )
 
 func main() {
-	// fmt.Println("Hello Friend")
-	// output, err := exec.Command("wget", `http://www.nairaland.com/`).Output()
-	// if err != nil {
-	// 	fmt.Println("Err:", err, string(output))
-	// 	return
-	// }
-
-	// fmt.Println(len(output), output)
-
-	res, err := http.Get("http://localhost:8500/v1/catalog/services")
+	output, err := exec.Command("iptables", "").Output()
 	if err != nil {
-		fmt.Println("Err: ", err)
+		fmt.Println("Err:", err, string(output))
 		return
 	}
 
-	defer res.Body.Close()
-	fmt.Println(res.StatusCode)
-	_, err = io.Copy(os.Stdout, res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(len(output), output)
+
+	// res, err := http.Get("http://localhost:8500/v1/catalog/services")
+	// if err != nil {
+	// 	fmt.Println("Err: ", err)
+	// 	return
+	// }
+
+	// defer res.Body.Close()
+	// fmt.Println(res.StatusCode)
+	// _, err = io.Copy(os.Stdout, res.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
