90e135ac9c9f18a6817ab673c8f46b4ce2a967ce
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-24 08:20:58 +0100

    remove vendor directory

--- vendor/github.com/ncodes/cocoon/core/cluster/cluster.go
+++
@@ -1,7 +0,0 @@
-package cluster
-
-// Cluster defines an interface for cluster interactions
-type Cluster interface {
-	Deploy(lang, url, tag string) (string, error)
-	SetAddr(addr string, https bool)
-}
