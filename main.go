645c1ca317389afe210a82e1f530386dc182d37e
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-16 09:41:36 +0100

    updated glide

--- main.go
+++ main.go
@@ -29,31 +29,10 @@ func print(err error, v interface{}) {
 
 // OnInvoke process invoke transactions
 func (app *App) OnInvoke(link *runtime.Link, txID, function string, params []string) (interface{}, error) {
+	log.Info("Invoke acknowledged")
 	return "success", nil
 }
 
 func main() {
 	runtime.Run(new(App))
-
-	// fmt.Println("Hello Friend")
-	// res, err := http.Get("https://google.com.ng")
-	// if err != nil {
-	// 	fmt.Println("Err: ", err)
-	// 	return
-	// }
-
-	// defer res.Body.Close()
-	// fmt.Println(res.StatusCode)
-	// _, err = io.Copy(os.Stdout, res.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// output, err := exec.Command("bash", "-c", "iptables -S").Output()
-	// if err != nil {
-	// 	fmt.Println("Err:", err, string(output))
-	// 	return
-	// }
-
-	// fmt.Println(len(output), string(output))
 }
