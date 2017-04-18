0f44f142a63fa0bcbbc50d069eb6795d9f46b98b
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-18 20:26:00 +0100

    remove vendor dir

--- vendor/github.com/ncodes/cocoon/core/runtime/golang/cocoon_code.go
+++
@@ -1,7 +0,0 @@
-package golang
-
-// CocoonCode defines the interface of a cocoon code.
-type CocoonCode interface {
-	OnInit(link *Link) error
-	OnInvoke(link *Link, txID, function string, params []string) (interface{}, error)
-}
