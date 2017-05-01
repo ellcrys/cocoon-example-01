cd7a929af41fa92b7e0a45c59fd37d4983dcd56b
Kennedy Idialu <kennedyidialu@gmail.com>
2017-05-01 13:20:54 +0100

    new update

--- main.go
+++ main.go
@@ -36,60 +36,9 @@ func (app *App) OnInvoke(header runtime.Metadata, function string, params []stri
 
 	fmt.Println("Found Function: ", function)
 	fmt.Println("Found Params: ", params)
+	fmt.Println("MY_VAR", os.Getenv("MY_VAR"))
+	fmt.Println("MY_VAR2", os.Getenv("MY_VAR2"))
 	pretty.Println(header)
-
-	// link.NewLedger("myledger2", true, true)
-	// log.Infof("Hello, I am %s", runtime.GetCocoonID())
-	// tx, err := link.Put("myledger2", "account.balance", []byte("10.20"))
-	// if err != nil {
-	// 	log.Info("failed to create transaction = ", err)
-	// } else {
-	// 	fmt.Println("Cur: ", tx)
-	// 	go func() {
-	// 		txRevised, err := link.PutSafe(tx.ID, "myledger2", "account.balance", []byte("20.40"))
-	// 		fmt.Println("Revised 1: ", txRevised, err)
-	// 	}()
-	// 	go func() {
-	// 		// time.Sleep(5 * time.Second)
-	// 		txRevised2, err := link.PutSafe(tx.ID, "myledger2", "account.balance", []byte("40.40"))
-	// 		fmt.Println("Revised 2: ", txRevised2, err)
-	// 	}()
-	// }
-
-	// start := time.Now()
-	// l2 := runtime.NewLink("u1")
-	// lock, err := l2.Lock("some_key", 2*time.Second)
-	// print(err, lock)
-	// err = lock.Acquire()
-	// print(err, nil)
-	// fmt.Println(time.Since(start))
-
-	// start = time.Now()
-	// lock2, err := l2.Lock("some_key", 10*time.Second)
-	// fmt.Println(time.Since(start))
-	// print(err, lock2)
-	// err = lock2.Acquire()
-	// print(err, nil)
-
-	// print(err, nil)
-	// err = lock.Release()
-	// print(err, nil)
-	// err = lock.IsAcquirer()
-	// print(err, nil)
-
-	// go func() {
-	// 	tx, err := link.Put("myledger2", "account.ben", []byte("10.20"))
-	// 	fmt.Print("Err 1: ")
-	// 	print(err, tx.ID)
-	// }()
-
-	// go func() {
-	// 	time.Sleep(8 * time.Second)
-	// 	fmt.Println("Getting.....")
-	// 	tx, err := link.Get("myledger2", "account.ben")
-	// 	fmt.Print("Err 2: ")
-	// 	print(err, tx)
-	// }()
 
 	return []byte("success"), nil
 }
